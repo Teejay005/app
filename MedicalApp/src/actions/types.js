7f414703c97aec590affb6aa8cf8a41d8170b81d
Francis Kiwana <fkiwana@thoughtworks.com>
2017-09-21 10:41:39 +1000

    Completed spike for speech to text translation

---
+++ MedicalApp/src/actions/types.js
@@ -0,0 +1,2 @@
+export const TRANSCRIBE_SPEECH = 'transcribe_speech';
+export const TRANSCRIPTION_COMPLETED = 'transcription_complete';
---
+++ MedicalApp/src/components/Main.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import VoiceTest from './VoiceTest';
+
+class Main extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <VoiceTest/>
+      </View>
+    );
+  }
+}
+
+
+const styles = {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+};
+
+// const mapStateToProps = ({ speech }) => {
+//   return { speech };
+// };
+//
+// export default connect(mapStateToProps, { translateText })(Main);
+export default Main;
