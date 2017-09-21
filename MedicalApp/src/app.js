5ea57ae3563b9fbc094d787d7a29d3d9ace3b8f9
Francis Kiwana <fkiwana@thoughtworks.com>
2017-09-21 14:19:44 +1000

    Added test page for voice recognition

--- MedicalApp/src/app.js
+++ MedicalApp/src/app.js
@@ -5,18 +5,13 @@ import {
   Text,
   View
 } from 'react-native';
+import VoiceTest from './components/VoiceTest';
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <VoiceTest/>
       </View>
     );
   }
